4de7fedec6fca4d0d713911d27552596bd1dc395
hiroHome <hirohiroherohero93@gmail.com>
2022-07-25 22:31:24 +0900

    basic type

---
+++ 1-TYPE/1-1-basic.ts
@@ -0,0 +1,31 @@
+{
+    /**
+     * JavaScript
+     * Primitive: number, string, boolean, symbol, undefined, null, bigint,
+     * Object: function, array.....
+     */
+
+    // number
+    const num: number = 2;
+
+    // string
+    const str: string = 'hello';
+
+    // boolean
+    const boal: boolean = true;
+
+    // undefined
+    // let name: undefined;
+    let age: number | undefined = undefined;
+    age = 8;
+    age = undefined;
+    function find(): number | undefined {
+        return undefined;
+    }
+
+    // null
+    // let person: null;
+    let person: string | null = null;
+    person = 'hi';
+    person = null;
+}
