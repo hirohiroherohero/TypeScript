d9a0bcaa478e7732986e9db050ea323adf484007
hiroHome <hirohiroherohero93@gmail.com>
2022-07-25 22:48:16 +0900

    basic type

--- 1-TYPE/1-1-basic.ts
+++ 1-TYPE/1-1-basic.ts
@@ -9,13 +9,13 @@
     const num: number = 2;
 
     // string
-    const str: string = 'hello';
+    const str: string = "hello";
 
     // boolean
     const boal: boolean = true;
 
     // undefined
-    // let name: undefined;
+    // let name: undefined; 💩
     let age: number | undefined = undefined;
     age = 8;
     age = undefined;
@@ -24,8 +24,36 @@
     }
 
     // null
-    // let person: null;
+    // let person: null; 💩
     let person: string | null = null;
-    person = 'hi';
+    person = "hi";
     person = null;
+
+    // unknown 💩
+    let notSure: unknown = 0;
+    notSure = "str";
+    notSure = true;
+
+    // any 💩
+    let anything: any = null;
+    anything = "hello";
+
+    // void: return undefined
+    function print(): void {
+        console.log("hello");
+        return;
+    }
+
+    // never: 절대 return x
+    function throwError(message: string): never {
+        // message -> server (log)
+        throw new Error(message);
+        // while (true) {...}
+    }
+
+    // object 💩
+    let obj: object;
+    function acceptSomeObject(obj: object) {}
+    acceptSomeObject({ name: "yang" });
+    acceptSomeObject({ animal: "dog" });
 }
