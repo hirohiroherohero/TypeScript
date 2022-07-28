5bf39d7005c6a4b6102ecbf33a6c53d4e2a65a02
hiroHome <hirohiroherohero93@gmail.com>
2022-07-28 23:38:34 +0900

    direction quiz

--- 2-TYPE-QUIZ/game.ts
+++ 2-TYPE-QUIZ/game.ts
@@ -1,12 +1,40 @@
 /**
  * Let's make a game 🕹
  */
-console.log(position); // { x: 0, y: 0}
-move('up');
-console.log(position); // { x: 0, y: 1}
-move('down');
-console.log(position); // { x: 0, y: 0}
-move('left');
-console.log(position); // { x: -1, y: 0}
-move('right');
-console.log(position); // { x: 0, y: 0}
+
+{
+    type DirectionType = "up" | "down" | "left" | "right";
+    type PositionType = {
+        x: number;
+        y: number;
+    };
+
+    const position: PositionType = { x: 0, y: 0 };
+
+    const move = (direction: DirectionType) => {
+        switch (direction) {
+            case "up":
+                position.y += 1;
+                break;
+            case "down":
+                position.y -= 1;
+                break;
+            case "left":
+                position.x -= 1;
+                break;
+            case "right":
+                position.x += 1;
+                break;
+        }
+    };
+
+    console.log(position); // { x: 0, y: 0}
+    move("up");
+    console.log(position); // { x: 0, y: 1}
+    move("down");
+    console.log(position); // { x: 0, y: 0}
+    move("left");
+    console.log(position); // { x: -1, y: 0}
+    move("right");
+    console.log(position); // { x: 0, y: 0}
+}
